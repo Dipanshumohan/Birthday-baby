9c551c2968e7e88c742bc7966336f1c77065a440
Dipanshu <118584345+Dipanshumohan@users.noreply.github.com>
2024-03-03 23:31:43 +0530

    added transition between screens

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 function switchScreen() {
-    var screen1 = document.getElementById('screen1');
-    var screen2 = document.getElementById('screen2');
-  
-    screen1.classList.toggle('active');
-    screen2.classList.toggle('active');
-  }
-  $(document).ready(function() {
-    $(".candles").click(function() {
-        $(".flame").animate({"opacity":0},"fast");
-        $(".flame2").animate({"opacity":0},"fast");
-        $(".flame3").animate({"opacity":0},"fast");
-        $(".text").animate({"opacity":1,"top":-90},"fast");
+    var body = document.body;
+    var cake = document.getElementById('birthday-cake');
+    var button = document.getElementById('button');
 
+    body.style.backgroundColor = 'pink'; // Change background color to pink
+
+    button.style.display = 'none'; // Hide the button
+
+    setTimeout(function () {
+        cake.classList.remove('hidden'); // Remove the 'hidden' class after 2 seconds
+        cake.classList.add('show'); // Add the 'show' class to reveal the cake with transition
+    }, 2000);
+
+
+}
+
+$(document).ready(function () {
+    $(".candles").click(function () {
+        $(".flame").animate({ "opacity": 0 }, "fast");
+        $(".flame2").animate({ "opacity": 0 }, "fast");
+        $(".flame3").animate({ "opacity": 0 }, "fast");
+        $(".text").animate({ "opacity": 1, "top": -90 }, "fast");
     });
-
 });
+
+
+
