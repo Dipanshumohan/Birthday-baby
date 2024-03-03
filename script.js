58a3170ed9a06d6dae31009872e6d133287c09df
Dipanshu <118584345+Dipanshumohan@users.noreply.github.com>
2024-03-03 22:30:45 +0530

    cake made

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function switchScreen() {
+    var screen1 = document.getElementById('screen1');
+    var screen2 = document.getElementById('screen2');
+  
+    screen1.classList.toggle('active');
+    screen2.classList.toggle('active');
+  }
+  $(document).ready(function() {
+    $(".candles").click(function() {
+        $(".flame").animate({"opacity":0},"fast");
+        $(".flame2").animate({"opacity":0},"fast");
+        $(".flame3").animate({"opacity":0},"fast");
+        $(".text").animate({"opacity":1,"top":-90},"fast");
 
+    });
+
+});
